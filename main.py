30be7b906b2e1c75b790c15a4e11fdf11d139a23
Aren Vanyan <i21.vanyan.aren@etud.ufar.am>
2026-02-17 17:20:32 +0400

    Added drawing logic

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import cv2, time
 import numpy as np
-import pyvirtualcam
+import math
+import utils as u
 
 
 
@@ -15,7 +16,10 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-index_finger_trajectory = []  # List to store index finger positions
+
+drawing_segments = []  # List to store segments of the drawing
+current_segment = []  # List to store the current segment being drawn
+not_drawing_frames = 0  # Counter for frames where drawing is not active
 
 # Create a hand landmarker instance with the live stream mode:
 def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
@@ -49,6 +53,48 @@ def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return
 
+def show_connections(frame, hand_landmarks):
+
+    h, w, _ = frame.shape
+    for landmark in hand_landmarks:
+        x_px = int(landmark.x * w)
+        y_px = int(landmark.y * h)
+        cv2.circle(frame, (x_px, y_px), 7, (0, 0, 255), -1)
+
+
+    connections = [
+        (0, 1), (1, 2), (2, 3), (3, 4),
+        (0, 5), (5, 6), (6, 7), (7, 8),
+        (5, 9), (9, 10), (10, 11), (11, 12),
+        (9, 13), (13, 14), (14, 15), (15, 16),
+        (0,17), (13, 17), (17, 18), (18, 19), (19, 20)
+    ]
+
+    for connection in connections:
+        start = hand_landmarks[connection[0]]
+        end = hand_landmarks[connection[1]]
+
+        x1, y1 = int(start.x * w), int(start.y * h)
+        x2, y2 = int(end.x * w), int(end.y * h)
+
+        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+def continue_drawing(frame, current_segment):
+    # Draw the saved segments
+    for segment in drawing_segments:
+        for i in range(1, len(segment)):
+            cv2.line(frame,
+                     segment[i - 1],  # first point
+                     segment[i],  # second point
+                     (0, 255, 0), 2)
+
+    for i in range(1, len(current_segment)):
+        cv2.line(frame,
+                 current_segment[i - 1],
+                 current_segment[i],
+                 (0, 255, 0), 2)
+
+
 
 
 # Create landmarker options
@@ -60,51 +106,69 @@ options = HandLandmarkerOptions(
 )
 
 # Open webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1) # change 0 to 1 if you have multiple cameras and want to use the second one
 
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = 30
-
-with pyvirtualcam.Camera(width=width, height=height, fps=fps) as cam:
-    with HandLandmarker.create_from_options(options) as landmarker:
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
+# Set camera resolution (change 640, 480 to your desired width and height)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 
-            frame = cv2.flip(frame, 1)
+with HandLandmarker.create_from_options(options) as landmarker:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-            # Convert to MediaPipe Image
-            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-            timestamp_ms = int(time.time() * 1000)
+        frame = cv2.flip(frame, 1)
 
-            result = landmarker.detect_for_video(mp_image, timestamp_ms)
+        # Convert to MediaPipe Image
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-            # Draw on frame
-            if result.hand_landmarks:
-                hand = result.hand_landmarks[0]
-                index_finger = hand[8]
+        timestamp_ms = int(time.time() * 1000)
 
-                h, w, _ = frame.shape
-                x_px = int(index_finger.x * w)
-                y_px = int(index_finger.y * h)
+        result = landmarker.detect_for_video(mp_image, timestamp_ms)
 
-                index_finger_trajectory.append((x_px, y_px))
+        # Draw on frame
+        if result.hand_landmarks:
+            show_connections(frame, result.hand_landmarks[0])
 
-                for i in range(1, len(index_finger_trajectory)):
-                    cv2.line(frame,
-                             index_finger_trajectory[i - 1],
-                             index_finger_trajectory[i],
-                             (0, 255, 0), 2)
+            hand = result.hand_landmarks[0]
+            index_finger_tip = hand[8]
+
+
+            h, w, _ = frame.shape
+            x_px = int(index_finger_tip.x * w)
+            y_px = int(index_finger_tip.y * h)
+
+
+            if u.activate_drawing(hand):
+                not_drawing_frames = 0
+                current_segment.append((x_px, y_px))
+
+                continue_drawing(frame, current_segment)
             else:
-                index_finger_trajectory.clear()
+                not_drawing_frames += 1
+                # When the current segment have points and drawing mode is off, save the segment
+                if current_segment:
+                    drawing_segments.append(current_segment)
+                    current_segment = []
 
-            # Convert BGR → RGB for virtual cam
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            cam.send(frame_rgb)
-            cam.sleep_until_next_frame()
+        else:
+            if len(current_segment) > 0:
+                drawing_segments.append(current_segment)
+                current_segment = []
+
+
+        cv2.imshow('Hand Landmarker', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
 
---
+++ utils.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+# mcp - 0
+# pip - 1
+# dip - 2
+# tip - 3
+fingers = {
+    'thumb': [1, 2, 3, 4],
+    'index': [5, 6, 7, 8],
+    'middle': [9, 10, 11, 12],
+    'ring': [13, 14, 15, 16],
+    'pinky': [17, 18, 19, 20],
+    'wrist': 0
+}
+
+def angle(j1, j2, j3):
+    # j1 = np.array([j1.x, j1.y, j1.z])
+    # j2 = np.array([j2.x, j2.y, j2.z])
+    # j3 = np.array([j3.x, j3.y, j3.z])
+
+    v1 = j1 - j2
+    v2 = j3 - j2
+
+    cos_ang = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+    angle_rad = np.arccos(cos_ang)
+    angle_deg = np.degrees(angle_rad)
+
+    return angle_deg
+
+def is_finger_straight(hand, finger_name) -> bool:
+    mcp = np.array([hand[fingers[finger_name][0]].x, hand[fingers[finger_name][0]].y, hand[fingers[finger_name][0]].z])
+    pip = np.array([hand[fingers[finger_name][1]].x, hand[fingers[finger_name][1]].y, hand[fingers[finger_name][1]].z])
+    dip = np.array([hand[fingers[finger_name][2]].x, hand[fingers[finger_name][2]].y, hand[fingers[finger_name][2]].z])
+    tip = np.array([hand[fingers[finger_name][3]].x, hand[fingers[finger_name][3]].y, hand[fingers[finger_name][3]].z])
+
+    angle_1 = angle(mcp, pip, dip)
+    angle_2 = angle(pip, dip, tip)
+
+    return angle_1 > 165 and angle_2 > 165
+
+def is_finger_bent(hand, finger_name) -> bool:
+
+    mcp = np.array([hand[fingers[finger_name][0]].x, hand[fingers[finger_name][0]].y, hand[fingers[finger_name][0]].z])
+    pip = np.array([hand[fingers[finger_name][1]].x, hand[fingers[finger_name][1]].y, hand[fingers[finger_name][1]].z])
+    dip = np.array([hand[fingers[finger_name][2]].x, hand[fingers[finger_name][2]].y, hand[fingers[finger_name][2]].z])
+    tip = np.array([hand[fingers[finger_name][3]].x, hand[fingers[finger_name][3]].y, hand[fingers[finger_name][3]].z])
+
+    angle_1 = angle(mcp, pip, dip)
+    angle_2 = angle(pip, dip, tip)
+
+    return angle_1 < 165 or angle_2 < 165
+
+# TODO + index tip higher than others (position)
+# TODO + index far from wrist (distance)
+# TODO + gesture stable for 5 frames
+def activate_drawing(hand) -> bool:
+    return (is_finger_straight(hand, 'index')
+            and is_finger_bent(hand, 'middle')
+            and is_finger_bent(hand, 'ring')
+            and is_finger_bent(hand, 'pinky')
+            and is_finger_bent(hand, 'thumb'))
+
+
