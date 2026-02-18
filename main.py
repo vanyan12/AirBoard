07a0709299843ddc24f1ea3bf8aac5aa71f5d413
Aren Vanyan <i21.vanyan.aren@etud.ufar.am>
2026-02-18 22:02:13 +0400

    Drawing smoothing and sound effect addition

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 import math
 import threading
 from utils import *
-import pygame
+import pygame, pyvirtualcam
 
 pygame.mixer.init()
 pygame.mixer.music.load("./assets/uuu.mp3")
@@ -121,101 +121,98 @@ options = HandLandmarkerOptions(
 )
 
 # Open webcam
-cap = cv2.VideoCapture(1) # change 0 to 1 if you have multiple cameras and want to use the second one
+cap = cv2.VideoCapture(0) # change 0 to 1 if you have multiple cameras and want to use the second one
 
 # Set camera resolution (change 640, 480 to your desired width and height)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = 30
+
+with pyvirtualcam.Camera(width=width, height=height, fps=fps) as cam:
+    with HandLandmarker.create_from_options(options) as landmarker:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
 
-with HandLandmarker.create_from_options(options) as landmarker:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
+            frame = cv2.flip(frame, 1)
+
+            # Convert to MediaPipe Image
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+            timestamp_ms = int(time.time() * 1000)
+
+            result = landmarker.detect_for_video(mp_image, timestamp_ms)
+
+            # Draw on frame
+            if result.hand_landmarks:
+                show_connections(frame, result.hand_landmarks[0])
+
+                hand = result.hand_landmarks[0]
+                index_finger_tip = hand[8]
 
 
-        frame = cv2.flip(frame, 1)
+                h, w, _ = frame.shape
+                x_px = int(index_finger_tip.x * w)
+                y_px = int(index_finger_tip.y * h)
 
-        # Convert to MediaPipe Image
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+                is_drawing_gesture = activate_drawing(hand)
 
-        timestamp_ms = int(time.time() * 1000)
+                # GESTURE STABILITY LOGIC
 
-        result = landmarker.detect_for_video(mp_image, timestamp_ms)
+                if is_drawing_gesture:
+                    drawing_stable_counter += 1
+                    not_drawing_stable_counter = 0
+                else:
+                    not_drawing_stable_counter += 1
+                    drawing_stable_counter = 0
 
-        # Draw on frame
-        if result.hand_landmarks:
-            show_connections(frame, result.hand_landmarks[0])
+                # START DRAWING (stable 5 frames)
+                if drawing_stable_counter == STABLE_FRAMES and not drawing_active:
+                    drawing_active = True
 
-            hand = result.hand_landmarks[0]
-            index_finger_tip = hand[8]
+                    threading.Thread(target=play_sound, daemon=True).start()
+
+                # END DRAWING (stable 5 frames)
+                if not_drawing_stable_counter == STABLE_FRAMES:
+                    drawing_active = False
+
+                    if len(current_segment) > 1:
+                        drawing_segments.append(current_segment)
+
+                    current_segment = []
+
+                # ACTUAL DRAWING
+                if drawing_active:
+                    if not current_segment:
+                        current_segment.append(smooth_point(x_px, y_px, None))
+                    else:
+                        last_x, last_y = current_segment[-1]
+                        distance = math.hypot(x_px - last_x, y_px - last_y)
+
+                        if distance > MIN_DIST:
+                            current_segment.append(smooth_point(x_px, y_px, current_segment[-1]))
 
 
-            h, w, _ = frame.shape
-            x_px = int(index_finger_tip.x * w)
-            y_px = int(index_finger_tip.y * h)
+                continue_drawing(frame, current_segment)
 
-            is_drawing_gesture = activate_drawing(hand)
-
-            # GESTURE STABILITY LOGIC
-
-            if is_drawing_gesture:
-                drawing_stable_counter += 1
-                not_drawing_stable_counter = 0
             else:
                 not_drawing_stable_counter += 1
+
                 drawing_stable_counter = 0
 
-            # START DRAWING (stable 5 frames)
-            if drawing_stable_counter == STABLE_FRAMES and not drawing_active:
-                drawing_active = True
+                if not_drawing_stable_counter == STABLE_FRAMES:
 
-                threading.Thread(target=play_sound, daemon=True).start()
+                    drawing_active = False
 
-            # END DRAWING (stable 5 frames)
-            if not_drawing_stable_counter == STABLE_FRAMES:
-                drawing_active = False
+                    if len(current_segment) > 1:
+                        drawing_segments.append(current_segment)
 
-                if len(current_segment) > 1:
-                    drawing_segments.append(current_segment)
+                    current_segment = []
 
-                current_segment = []
-
-            # ACTUAL DRAWING
-            if drawing_active:
-                if not current_segment:
-                    current_segment.append(smooth_point(x_px, y_px, None))
-                else:
-                    last_x, last_y = current_segment[-1]
-                    distance = math.hypot(x_px - last_x, y_px - last_y)
-
-                    if distance > MIN_DIST:
-                        current_segment.append(smooth_point(x_px, y_px, current_segment[-1]))
-
-
-            continue_drawing(frame, current_segment)
-
-        else:
-            not_drawing_stable_counter += 1
-
-            drawing_stable_counter = 0
-
-            if not_drawing_stable_counter == STABLE_FRAMES:
-
-                drawing_active = False
-
-                if len(current_segment) > 1:
-                    drawing_segments.append(current_segment)
-
-                current_segment = []
-
-        cv2.imshow('Hand Landmarker', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+            cam.send(frame)
+            cam.sleep_until_next_frame()
 
 
 
